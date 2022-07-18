9f48f5b76477ec710eb3316c0d2943cb339629d8
Dale Evans <dale.evans@fwi.com>
2022-07-18 08:45:14 +0100

    Added room to app

--- app/src/main/java/com/example/sleeptracker/SleepTrackerApplication.kt
+++ app/src/main/java/com/example/sleeptracker/SleepTrackerApplication.kt
@@ -1,7 +1,8 @@
 package com.example.sleeptracker
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SleepTrackerApplication {
+class SleepTrackerApplication : Application() {
 }
---
+++ app/src/main/java/com/example/sleeptracker/SleepTrackerVM.kt
@@ -0,0 +1,10 @@
+package com.example.sleeptracker
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SleepTrackerVM @Inject constructor() : ViewModel() {
+
+}
